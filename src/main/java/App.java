5b9fde1765e1a7150cbb4cd9c472772eb687b9a1
Daren Schaad and Jake Steinberg <darenschaad@gmail.com>
2016-02-10 15:50:58 -0800

    add a user webpage for the user to enter a sentence, enter a word to change, and what to change it to, and display the altered sentence on a new page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,23 +11,25 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-    // String layout = "templates/layout.vtl";
-    //
-    // get("/", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/home.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/detector", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/detector.vtl");
-    //
-    //   String playerOneThrow = request.queryParams("optionsRadios");
-    //   String winner = defineWinner(playerOneThrow, computerChooses());
-    //   model.put("winner", winner);
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/detector", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/detector.vtl");
+
+      String userSentence = request.queryParams("sentence");
+      String wordToReplace = request.queryParams("replaceWord");
+      String replaceWith = request.queryParams("replaceWith");
+      String alteredSentence = replaceWord(userSentence, wordToReplace, replaceWith);
+      model.put("alteredSentence", alteredSentence);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 
 
